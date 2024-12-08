23ff6d5ac208018b79041ae114e5cae8f5798b3e
Amine Arbi <aminearbi@users.noreply.github.com>
2024-12-08 05:40:42 +0100

    edited: styling and colors

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run(port=8080,host="0.0.0.0", debug=True)
